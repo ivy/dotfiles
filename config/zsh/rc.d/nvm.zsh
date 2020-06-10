653826404cb6f5d1c1f8bcdc38b8cc1ce0fac6ff
Ivy Evans <ivy@ivyevans.net>
2020-06-09 18:16:55 -0700

    zsh: rc: Automatically select Node version

--- config/zsh/rc.d/nvm.zsh
+++ config/zsh/rc.d/nvm.zsh
@@ -2,6 +2,29 @@
 #
 # See https://github.com/nvm-sh/nvm#readme
 
+# load-nvmrc switches the NVM Node version based on the current working
+# directory. Based on https://stackoverflow.com/a/39519460
+load-nvmrc() {
+  local current_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [[ -z "$nvmrc_path" ]] && \
+     [[ "$current_version" != "$(nvm version default)" ]];
+  then
+    echo 'Switching to default NVM version.'
+    nvm use default
+    return
+  fi
+
+  local nvmrc_version="$(nvm version "$(<"$nvmrc_path")")"
+
+  if [[ "$nvmrc_version" = "N/A" ]]; then
+    nvm install
+  elif [[ "$nvmrc_version" != "$current_version" ]]; then
+    nvm use
+  fi
+}
+
 function {
   local search_dirs=(
     "$HOME/.nvm" /usr/local/opt/nvm
@@ -14,6 +37,10 @@ function {
 
     source "$dir/nvm.sh"
     source "$dir/etc/bash_completion.d/nvm"
+
+    autoload -U add-zsh-hook
+    add-zsh-hook chpwd load-nvmrc
+
     break
   done
 }
