5311db65fa19887bd2ecd5543c619d75c94f8d13
Ivy Evans <ivy.evans@gusto.com>
2022-10-06 16:15:54 -0700

    Add git-duet

---
+++ Brewfile
@@ -0,0 +1,1 @@
+brew 'git-duet/tap/git-duet'
---
+++ config/zsh/env.d/git-duet.zsh
@@ -0,0 +1,3 @@
+# Enable "Co-authored-by" trailer support
+# See https://github.com/git-duet/git-duet#co-authored-by-trailer-support
+GIT_DUET_CO_AUTHORED_BY=1
