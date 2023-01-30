d7e87915c0c95a059d643b3336721b409aea0780
Ivy Evans <ivy.evans@gusto.com>
2023-01-30 12:15:32 -0800

    zsh: env: Move local path setup to end of init

--- config/zsh/env.d/zz-local-path.zsh
+++ config/zsh/env.d/zz-local-path.zsh
@@ -1,8 +1,8 @@
 # Reset OSX $PATH with system-wide defaults.
-if [[ "$OSTYPE" == darwin* ]]; then
-  export PATH=""
-  eval `/usr/libexec/path_helper -s`
-fi
+#if [[ "$OSTYPE" == darwin* ]]; then
+#  export PATH=""
+#  eval `/usr/libexec/path_helper -s`
+#fi
 
 # Add ~/bin to executables path.
 export PATH="$HOME/bin:$PATH"
