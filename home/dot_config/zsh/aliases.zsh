7f2dca0d5a41ee24dbb5a1989df124eae01f384a
Martin Emde <me@martinemde.com>
2025-09-06 21:30:58 -0700

    feat: update zsh configuration and add personal aliases
    - Set VISUAL to cursor command in dot_zshenv
    - Change ZSH_THEME to garyblessington in dot_zshrc.tmpl
    - Enable case-sensitive completion in dot_zshrc.tmpl
    - Add personal aliases in new aliases.zsh file for convenience

---
+++ home/dot_config/zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# Personal aliases
+alias vi="vim"
+alias g="git"
+alias tf=terraform
+
+alias ll="ls -lhA"
+alias la="ls -A"
+alias l="ls"
+
+alias nerd="mvim +NERDTree"
+alias mvimdiff="mvim -d"
+
+# Ruby/Rails aliases
+alias b="bundle exec"
+alias r="bundle exec rspec"
+alias rof="bundle exec rspec --only-failures"
+alias rff="bundle exec rspec --fail-fast"
+
+# For `dbundle exec ruby script_that_needs_bundler_from_git.rb`
+alias dbundle="ruby -I/Users/martinemde/Projects/github.com/rubygems/rubygems/bundler/lib /Users/martinemde/Projects/github.com/rubygems/rubygems/bundler/exe/bundle"
+
+alias du1='du -h --max-depth=1'
+alias fn='find . -name'
+alias hi='history | tail -20'
